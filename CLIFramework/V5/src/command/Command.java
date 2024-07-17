ead3c529797de96104ab1147686e8ac3e5bb0d3e
Garden-Yoon <jungwon.yoon94@gmail.com>
2024-07-17 17:53:56 +0900

    240717_1753 학생컴퓨터 커밋

---
+++ CLIFramework/V5/src/command/Command.java
@@ -0,0 +1,6 @@
+package command;
+
+public interface Command {
+    // 추상메소드이므로 재정의 필요
+    void execute();
+}
---
+++ CLIFramework/V5/src/command/InitScoresCommand.java
@@ -0,0 +1,19 @@
+package command;
+
+import basic.Input;
+import domain.StudentScores;
+
+// 1번 메뉴인 학생 수 입력 기능 처리하는 클래스
+public class InitScoresCommand implements Command {
+    StudentScores studentScores = StudentScores.getInstance();
+
+    @Override
+    public void execute() {
+//        getInt를 사용해서 사용자한테 학생 수를 입력받음
+        int studentNum = Input.getInt("학생수> ");
+        // studentNum과 점수 배열의 크기를 할당
+        studentScores.setStudentNum(studentNum);
+    }
+
+
+}
